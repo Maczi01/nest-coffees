23e2c5986a0ce6df31e097a0e27f9fae8b0cf299
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-14 21:34:44 +0100

    feat: added logger

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller('coffees')
 @ApiTags('coffees')
 export class CoffeesController {
+  private readonly logger = new Logger(CoffeesController.name);
+
   @Get()
   findAll() {
-    return 'GET request 1';
+    this.logger.log('Find all invoked');
+    return 'GET request 2';
   }
 }
