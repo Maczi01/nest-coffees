24fa1a342259694fb789ac66baa4679922b10886
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-14 22:24:56 +0100

    feat: updated method patch, delete

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,13 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Logger,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
@@ -30,8 +32,20 @@ export class CoffeesController {
   }
 
   @Post()
-  @HttpCode(HttpStatus.OK)
+  @HttpCode(HttpStatus.CREATED)
   createCoffee(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  updateCoffee(@Param('id') id: string, @Body() body) {
+    return `Element with ${id} will be updated with ${body.name}`;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  removeCoffee(@Param('id') id: string) {
+    return `Element with ${id} will be removed with`;
   }
 }
