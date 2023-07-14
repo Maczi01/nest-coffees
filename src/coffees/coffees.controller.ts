630b18ab2175ba75438c041fa5098d03700d102b
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-14 21:39:00 +0100

    feat: added endpoint to get one coffee

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import {Controller, Get, Logger, Param} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller('coffees')
@@ -10,5 +10,10 @@ export class CoffeesController {
   findAll() {
     this.logger.log('Find all invoked');
     return 'GET request 2';
+  }
+  @Get(':id')
+  findById(@Param() params) {
+    this.logger.log(`Get item with param ${params.id}`);
+    return `Get item with param ${params.id}}`;
   }
 }
