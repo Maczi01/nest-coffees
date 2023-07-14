719c20f39fcc051d28cd3515ee2dc533d58c0ecc
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-14 20:34:55 +0100

    chore: created dockerfile

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('coffees')
+@ApiTags('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
-    return 'GET request';
+    return 'GET request 1';
   }
 }
