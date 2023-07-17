0e180880557ec151d17b0973d91d9047057747f1
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-17 16:45:03 +0100

    chore: linting

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,7 +3,7 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
-import {Flavor} from "./entities/flavor.entity";
+import { Flavor } from './entities/flavor.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
