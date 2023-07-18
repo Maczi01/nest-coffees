2b9eca2e8926fa5c1f1a87828ccf720a95aa279b
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-19 00:09:13 +0100

    feat: handle migration

---
+++ typeorm-cli.config.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import {CoffeeRefactor1689721357962} from "./src/migrations/1689721357962-CoffeeRefactor";
+
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '123',
+  database: 'postgres',
+  entities: [],
+  migrations: [CoffeeRefactor1689721357962],
+});
